4a07d05a32b508a8f5142e0f35afc869d8da60e8
Lechufane <diego.villafane94@gmail.com>
2021-09-23 23:33:36 -0300

    hola

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+function App(){
+    const [bioState, setStateBio]= useState(false);
+
+const displayBio = () =>{
+      setStateBio(!bioState)
+    }
+
+
+let bio = (
+      <div>
+        <p>I'm from Mendoza, Argentina.</p>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          I code every day, my favorite languaje is JavaScript, and I made this
+          single page web portfolio with ReactJS
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+        <p>
+          I also enjoy travels, games, videogames, comics, and music. Well, pop
+          culture in general!
+        </p>
+      </div>
+    );
+
+useEffect(()=>{
+  setStateBio(true)
+})
+  
+    return (
+      <div>
+        <h1>Hello! My name is Diego.</h1>
+        <p>I'm a junior Front-End developer.</p>
+        <p>Been learning for 3 years now.</p>
+        {bioState? bio : ""}
+        <button onClick={()=> setStateBio(displayBio)}>{bioState? "Show less": "Read more"}</button>
+      </div>
+    );
+  }
 
 export default App;
